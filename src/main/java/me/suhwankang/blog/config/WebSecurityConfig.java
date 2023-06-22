5aacbaea892a29321ada74b1ddfb89b6e2337b09
suhwankang <suhwankang@gmail.com>
2023-06-22 20:55:54 +0900

    20220622 수정 반영

---
+++ src/main/java/me/suhwankang/blog/config/WebSecurityConfig.java
@@ -0,0 +1,46 @@
+package me.suhwankang.blog.config;
+
+import lombok.RequiredArgsConstructor;
+import me.suhwankang.blog.service.UserDetailService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebSecurityConfig {
+
+    private final UserDetailService userDetailService;
+
+    @Bean
+    public WebSecurityCustomizer configure(){
+        return (web) -> web.ignoring()
+                .requestMatchers(toH2Console())
+                .requestMatchers("/static/**");
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        return http
+                .authorizeRequests()
+                .requestMatchers("/login","/signup","/user").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/articles")
+                .and()
+                .logout()
+                .logoutSuccessUrl("/login")
+                .invalidateHttpSession(true)
+                .and()
+                .csrf().disable()
+                .build();
+
+
+    }
+}
